5e1192bb1dfbfe309a752cda9bca671cb38639b3
AbuSparky <abusparky007@gmail.com>
2025-05-07 10:43:34 +0530

    nlink change

--- src/common.js
+++ src/common.js
@@ -13,55 +13,55 @@ export const PROJECTS = [
         description: "This is a basic online shopping website built to simulate a real-world e-commerce experience. It allows users to browse a catalog of products, view detailed product information, add items to a shopping cart, and proceed to a basic checkout flow. The site is designed with a clean, user-friendly interface and includes essential e-commerce functionalities like product listings, cart management, and responsive design for mobile compatibility. .",
         image: img1,
         stack: ["React", "MongoDb","Bootstrap","Express","CSS"],
-        nlink: "ecommerce01.netlify.app"
+        nlink: "https://ecommerce01.netlify.app"
     },
     {
         title: "Inventory Management System",
         description: "This Inventory Management System is a lightweight, user-friendly application developed using React for efficient tracking and management of products in stock. It provides a centralized dashboard where users can view, add, update, and delete inventory items in real time. The application is designed to simplify inventory handling for small businesses or personal use.",
         image: img2,
         stack: ["CSS","Bootstrap", "React", "FireBase"],
-        nlink: "cookwaredemo.netlify.app"
+        nlink: "https://cookwaredemo.netlify.app"
     },
     {
         title: "Tips Calculator",
         description: "This Tip Calculator is a lightweight web application designed to help users easily calculate the tip amount and the total cost of a bill based on a custom tip percentage. Built with a focus on usability and responsiveness, it offers a smooth user experience across devices. Users can enter the bill amount, select or input a tip percentage, and see the calculated tip and total instantly.",
         image: img3,
         stack: ["React","CSS"],
-        nlink: "reacttipcalculator001.netlify.app"
+        nlink: "https://reacttipcalculator001.netlify.app"
     },
     {
         title: "JSKA Maldives",
         description: "This website was created for the JSKA Maldives Karate Association to share information with members, students, and visitors. It includes details about the management team, instructors, and the overall structure of the association. There's also a photo gallery showing events, training sessions, and tournaments. The site is easy to use, mobile-friendly, and designed to reflect the spirit and discipline of karate.",
         image: img4,
         stack: ["HTML", "CSS", "Javascript","Firebase"],
-        nlink: "jskamaldives.netlify.app"
+        nlink: "https://jskamaldives.netlify.app"
     },
     {
         title: "Match Game",
         description: "This is a simple browser-based match game where players flip two cards at a time to find matching pairs. Designed for casual play and entertainment, it features colorful tech logos and smooth animations. It’s a great way to relax while sharpening your memory skills!",
         image: img5,
         stack: ["HTML", "CSS", "Javascript"],
-        nlink: "matchgame01.netlify.app"
+        nlink: "https://matchgame01.netlify.app"
     },
     {
         title: "Age Calculator",
         description: "This is a simple web tool that helps users calculate their age based on their date of birth. The user inputs their birthdate, and the calculator instantly shows their current age in years, months, and days. It’s easy to use, accurate, and works on both desktop and mobile devices.",
         image: img6,
         stack: ["HTML", "CSS", "Javascript"],
-        nlink: "agecalculate001.netlify.app"
+        nlink: "https://agecalculate001.netlify.app"
     },
     {
         title: "Profile Reviews",
         description: "This is a simple browser-based match game where players flip two cards at a time to find matching pairs. Designed for casual play and entertainment, it features colorful tech logos and smooth animations. It’s a great way to relax while sharpening your memory skills!",
         image: img7,
         stack: ["HTML", "CSS", "Javascript"],
-        nlink: "reviews001abu.netlify.app"
+        nlink: "https://reviews001abu.netlify.app"
     },
     {
         title: "Student Certificate Management System",
         description: "Student Certificate Management System is a web-based application designed to streamline the process of creating, managing, and issuing academic certificates for students. It allows administrators to generate certificates digitally, store them securely, and provide students with easy access and verification options.",
         image: img8,
         stack: ["HTML" , "CSS" , "JavaScript" , "FireBase"],
-        nlink: "student-certificate-management-system.netlify.app"
+        nlink: "https://student-certificate-management-system.netlify.app"
     }
 ]
